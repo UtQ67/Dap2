27d9b9ba78ea20f58e353ee9bd4d2c582c61b73b
UtQ67 <pazarciutku@gmail.com>
2019-04-29 22:02:51 +0200

    .

--- Blatt04/ConvexHull.java
+++ Blatt04/ConvexHull.java
@@ -5,11 +5,46 @@ class ConvexHull
 {
 
     public static void main(String[] args) {
-        Point[] p = {new Point(10d,10d), new Point(100d, 10d), new Point(10d,100d)};
-        ConvexHull c = new ConvexHull();
-        List<Point> hull = c.simpleConvex(p);
 
-        for (Point pw : hull) {
+        ConvexHull c = new ConvexHull();
+        Point[] sample = new Point[]{new Point(10d,10d), new Point(100d, 10d), new Point(10d,100d)};
+        List<Point> searchedHull = c.simpleConvex(sample);
+
+        Point[] p = new Point[1003];
+        p[0] = new Point(10d,10d);
+        p[1] = new Point(100d, 10d);
+        p[2] =  new Point(10d,100d);
+
+        Random rand = new Random();
+        for(int i = 3; i < 1003; )
+        {
+            System.out.println("Searching " + i);
+
+            // Punkte von der beispiel Hülle kopieren
+            Point[] hullTest = new Point[sample.length + 1];
+            for(int j = 0; j < sample.length; j++)
+                hullTest[j] = sample[j];
+
+            // Generieren des zufälligen Punktes welches in der Konvexen Hülle sein soll
+            hullTest[hullTest.length - 1] = new Point(2, 11, 11);
+            
+            List<Point> hullP = c.simpleConvex(hullTest);
+
+            // überprüfen on der Punkt in der Konvexen Hülle ist
+            boolean isInsideSearched = true;
+            for (Point var : hullP) {  
+                System.out.println(var);
+                if(!searchedHull.contains(var))
+                    isInsideSearched = false;
+            }
+            if(isInsideSearched)
+            {
+                p[i] = hullTest[hullTest.length - 1];
+                i++;
+            }
+        }
+
+        for (Point pw : c.simpleConvex(p)) {
             System.out.println(pw.get(0) + " " + pw.get(1));
         }
     }
@@ -25,16 +60,18 @@ class ConvexHull
                 smallestPointIndex = i;
         }
         convexHull.add(p[smallestPointIndex]);
-
+        
+        // Wenn man nur einen Punkt hat, besteht die Konvexe Hülle nur aus dem Punkt
+        if(p.length == 1)
+            return convexHull;
      
-        // simple convex aus der Vorlesung
+        // last Point wird verwendet um von dort aus eine gerade zu ziehen, um zu pberprüfen ob die restlichen Punkte rechts der geraden liegen
         Point lastPoint = convexHull.peekLast();
         while(convexHull.size() == 1  || convexHull.peekFirst() != convexHull.peekLast())
         {
-       
+            // p[i] ist der 2. Punkt mit der die gerade konstruiert wird
             for(int i = 0; i < p.length; i++)
             {
-                System.out.println(i);
                 
                 // die betrachtetetn Punkte dürfen nicht gleich sein
                 if(lastPoint == p[i])
@@ -64,20 +101,7 @@ class ConvexHull
 
     private boolean isLeft(Point p1, Point p2, Point p)
     {
+        // z-Komponente vom Kreuzprodukt von P1P2 und P1P
         return (p2.get(0) - p1.get(0)) * (p.get(1) - p1.get(1)) - (p2.get(1) - p1.get(1)) * (p.get(0) - p1.get(0)) > 0;
-    }
-
-    public static double getRandom(){
-
-        //  Generator erzeugen
-        java.util.Random generator = new java.util.Random();
-
-        //  Grenzen festlegen
-        int lower = -1000;
-        int upper = 1000;
-
-        //  Zufallszahl erzeugen und zurückgeben
-        double random = lower + (upper+1 - lower) * generator.nextDouble();
-        return random;
     }
 }
