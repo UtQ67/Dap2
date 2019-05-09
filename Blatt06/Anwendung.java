72aebbb542d335039745768c954250968a0b09b3
UtQ67 <pazarciutku@gmail.com>
2019-05-09 21:22:29 +0200

    kommentare

--- Blatt06/Anwendung.java
+++ Blatt06/Anwendung.java
@@ -16,19 +16,21 @@ class Anwendung
     public static final String JOB_STRING = "Job";
 
     public static void main(String[] args) {
-
+        // Parameter einlesen
         String mode = null;
         BufferedReader file = null;
         try {
+            // Man benötigt genau 2 Parameter
             if(args.length != 2)
                 throw new InvalidParameterException();
+            // Job oder Interval
             if(args[0].equals(INTERVAL_STRING))
                 mode = INTERVAL_STRING;
             else if(args[0].equals(JOB_STRING))
                 mode = JOB_STRING;
             else
                 throw new InvalidParameterException();
-
+            // Datei erzeugen
             file = new BufferedReader(new FileReader(args[1]));
 
         }catch (Exception e)
@@ -37,6 +39,7 @@ class Anwendung
             System.exit(0);
         }
         System.out.println("Bearbeitete Datei \""+ args[1] + "\"");
+        // Auswahl von job / Interval abhängig von der eingabe
         if(mode.equals(INTERVAL_STRING))
             interval(file);
         else
@@ -46,6 +49,7 @@ class Anwendung
 
     private static void interval(BufferedReader file)
     {
+        // Einlesen der intervalle von der datei
         ArrayList<Interval> intervals = new ArrayList<>();
 
         try{
@@ -58,16 +62,19 @@ class Anwendung
             }
         }catch(Exception e)
         {
-            System.out.println("Etwas ist schief gegangen");
+            System.out.println("Etwas ist schief gegangen. Datei konnte nicht eingelesen weren");
         }
-                
+         
+        // Ausgabe
         System.out.print("Es wurden " + intervals.size() + " Intervalle eingelesen: ");
         printIntervalList(intervals);
 
+        // Sortieren + Ausgabe
         System.out.print("Sortiert: ");
         Collections.sort(intervals);
         printIntervalList(intervals);
 
+        // Scheduling + ausgabe
         System.out.print("Berechnetes Intervalscheduling: ");
         ArrayList<Interval> schedule = intervalScheduling(intervals);
         printIntervalList(schedule);
@@ -75,6 +82,7 @@ class Anwendung
 
     private static void job(BufferedReader file)
     {
+        // Jobs einelesen aus der geegebenen datei
         ArrayList<Job> jobs = new ArrayList<>();
 
         try{
@@ -87,16 +95,19 @@ class Anwendung
             }
         }catch(Exception e)
         {
-            System.out.println("Etwas ist schief gegangen");
+            System.out.println("Etwas ist schief gegangen. Datei konnte nicht eingelesen werden");
         }
-                
+        
+        // Ausgabe
         System.out.print("Es wurden " + jobs.size() + " Jobs eingelesen: ");
         printJobList(jobs);
 
+        // Sortieren + ausgabe
         System.out.print("Sortiert: ");
         Collections.sort(jobs);
         printJobList(jobs);
 
+        // Scheduling + ausgabe
         System.out.print("Berechnetes Latenessscheduling: ");
         int[] schedule = latenessScheduling(jobs);
         System.out.print("[");
@@ -108,13 +119,17 @@ class Anwendung
         }
         System.out.println("]");
 
+        // berechnung maximale verspätung + ausgabe
         System.out.print("Berechnete maximale verspätung: ");
         int versp = schedule[schedule.length-1] + jobs.get(jobs.size()-1).getLength() - jobs.get(jobs.size()-1).getDeadline();
+        
+        // wenn die vorher berechnete verspätung negativ isst, gibt es keine verspätung. Job wird vor deadline fertiggestellt
         versp = versp <= 0 ? 0 : versp;
 
         System.out.println(versp);
     }
 
+    // Ausgabe einer interval arraylist
     public static void printIntervalList(ArrayList<Interval> intervals)
     {
         System.out.print("[");
@@ -126,6 +141,8 @@ class Anwendung
         }
         System.out.println("]");
     }
+
+    // Ausgabe einer Job arraylist
     public static void printJobList(ArrayList<Job> jobs)
     {
         System.out.print("[");
@@ -138,9 +155,10 @@ class Anwendung
         System.out.println("]");
     }
 
-
+    // Greedy Algorithmus für interval scheduling problem
     public static ArrayList<Interval> intervalScheduling(ArrayList<Interval> intervals)
     {
+        // kein interval vorhanden -> kein scheduling möglich
         if (intervals == null)
             return null;
         if(intervals.size() == 0)
@@ -162,6 +180,8 @@ class Anwendung
 
         return ret;
     }
+
+    // lateness scheduling algorithmus
     public static int[] latenessScheduling(ArrayList<Job> jobs)
     {
         int[] schedule = new int[jobs.size()];
